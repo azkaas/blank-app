4aa63f68ee758db0be6d3a01158883337ef6a065
azkaas <workingazkaa@gmail.com>
2026-05-12 08:54:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("kalkulator gravimetri")
+st.title("Kalkulator Gravimetri")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
